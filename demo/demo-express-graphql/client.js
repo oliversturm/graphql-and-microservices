4ee3a5e124a7225e877f07ce004508e3dcbb797a
Oli Sturm <oliver+commitsigning@oliversturm.com>
2024-06-28 22:07:15 +0100

    feat: use apollo client

--- demo/demo-express-graphql/client.js
+++ demo/demo-express-graphql/client.js
@@ -1,16 +1,22 @@
-const { request } = require('graphql-request');
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client/core/index.js";
 
-request(
-  'http://localhost:4000/graphql',
-  /* GraphQL */ `
-    {
-      countries(searchNames: ["island"]) {
-        name
-        areaKM2
-        # areaPerPerson
-      }
-    }
-  `
-).then(result => {
-  console.log(result);
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
 });
+
+client
+  .query({
+    query: gql`
+      {
+        countries(searchNames: ["island"]) {
+          name
+          areaKM2
+          # areaPerPerson
+        }
+      }
+    `,
+  })
+  .then((result) => {
+    console.log(result.data.countries);
+  });
